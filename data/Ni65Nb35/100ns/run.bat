c7b07a307515419fd61437573e5150a0d4686d7e
EBocats <d202280113@hust.edu.cn>
2024-06-30 00:18:36 +0800

    Ni65Nb35

---
+++ data/Ni65Nb35/100ns/run.bat
@@ -0,0 +1,15 @@
+#!/bin/bash
+mpirun -np 48 lmp_intel -i dms600.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms580.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms560.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms540.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms520.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms500.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms480.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms460.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms440.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms420.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms400.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms380.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms360.lmp -sf intel
+mpirun -np 48 lmp_intel -i dms340.lmp -sf intel
